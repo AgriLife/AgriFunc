4bd38f0a0bcf24ca46a45baf4ab78e854ab43bdf
J. Aaron Eaton <aaron@channeleaton.com>
2013-05-30 16:23:30 -0500

    Put a safety wrapper on the accordion class

--- lib/Shortcode/Accordion.php
+++ lib/Shortcode/Accordion.php
@@ -3,29 +3,31 @@
  * Original code by Patrick Forringer (http://patrick.forringer.com)
  */
 
-class Shortcode_Accordion{
-	
-	public function __construct(){
+if ( ! class_exists( Shortcode_Accordion ) ) {
+	class Shortcode_Accordion{
+		
+		public function __construct(){
 
-		add_shortcode( 'accordion', array( $this, 'do_shortcode' ) );
-		
-	}
-	
-	public function do_shortcode( $attrs, $content ){
-		
-		$attrs = shortcode_atts(array(
-				'title' => 'Expand Content',
-				'group' => get_the_ID()
-			), $attrs );
+			add_shortcode( 'accordion', array( $this, 'do_shortcode' ) );
 			
-		$op = (object) apply_filters( 'accordion_attributes', $attrs );
+		}
+		
+		public function do_shortcode( $attrs, $content ){
+			
+			$attrs = shortcode_atts(array(
+					'title' => 'Expand Content',
+					'group' => get_the_ID()
+				), $attrs );
+				
+			$op = (object) apply_filters( 'accordion_attributes', $attrs );
 
-		ob_start();
-		require AF_DIR . '/views/accordion.php';
-		$accordion = ob_get_contents();
-		ob_clean();
+			ob_start();
+			require AF_DIR . '/views/accordion.php';
+			$accordion = ob_get_contents();
+			ob_clean();
 
-		return $accordion;
+			return $accordion;
+		}
+
 	}
-
 }
